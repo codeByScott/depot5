34ff895cea930b293c460ad7a05c28b918620625
codeByScott <scott@codebyscott.io>
2017-06-08 12:30:23 +0900

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -10,4 +14,14 @@ class Product < ApplicationRecord
   def self.latest
     Product.order(:updated_at).last
   end
+
+  private
+
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line items present')
+        throw :abort
+      end
+    end
 end
