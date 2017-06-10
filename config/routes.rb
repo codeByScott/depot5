f6285a2d0678942c1111d00b2f21bb5c364def7a
codeByScott <scott@codebyscott.io>
2017-06-10 18:09:20 +0900

    add mailers

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
-  resources :products
+  root 'store#index', as: 'store_index'
 
-  root 'store#index', as: 'store'
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
