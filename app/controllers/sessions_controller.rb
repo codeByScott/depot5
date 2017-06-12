823084f8a6a3d3d4bfaffa8f387e6de2be7e9ded
codeByScott <scott@codebyscott.io>
2017-06-12 09:16:50 +0900

    fix logout bug and style error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:users_id] = nil
+    session[:user_id] = nil
     redirect_to store_index_url, notice: "Logged Out"
   end
 end
